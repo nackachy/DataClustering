0488e8dbb47150e8d0b56f4015f9cc07aa65d741
nackachy <30370027+nackachy@users.noreply.github.com>
2019-04-01 12:46:14 +0100

    version 1.0

--- src/main/scala/com/DataVectis/Clustering/MainCluster.scala
+++ src/main/scala/com/DataVectis/Clustering/MainCluster.scala
@@ -1,7 +1,10 @@
 package com.DataVectis.Clustering
 
-import org.apache.spark.sql.SparkSession
+import java.io.File
+
+import org.apache.spark.sql.{SaveMode, SparkSession}
 import java.util.regex.Pattern
+
 import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.fs.{FileSystem, Path}
 
@@ -18,7 +21,7 @@ object MainCluster {
 
 
     //Getting path from "application.properties" using the method "Prop" from "Properties.scala"
-    val inputData = new prop
+    val inputData,outPutData = new prop
 
     // Importing data
     val dataset = spark.read.json(inputData.getProp("inputData"))
@@ -34,6 +37,40 @@ object MainCluster {
 
     // Displaying Data Clussified
     clusters.drop("features").show()
+
+    //Checking if output exists
+    val checkOutputDataPath = new File(outPutData.getProp("outPutData")).mkdir()
+
+    if (checkOutputDataPath) {
+      // Saving the dataset with labels
+
+      clusters
+        .repartition(1)
+        .write
+        .mode(SaveMode.Overwrite)
+        .format("com.databricks.spark.csv")
+        .option("header", "true")
+        .option("delimiter", ";")
+        .save(outPutData.getProp("outPutData"))
+
+      println("Data saved in : "+outPutData.getProp("outPutData"))
+    } else{
+
+      println("OutPut Data Path Not Found")
+      println("Creatin Data Path Out Put")
+      new File(outPutData.getProp("outPutData"))
+
+      clusters
+        .repartition(1)
+        .write
+        .mode(SaveMode.Overwrite)
+        .format("com.databricks.spark.csv")
+        .option("header", "true")
+        .option("delimiter", ";")
+        .save(outPutData.getProp("outPutData"))
+
+      println("Data saved in : "+outPutData.getProp("outPutData"))
+    }
 
     // Ploting Individuals according to Clusters laels
     val plot = new DataViz
